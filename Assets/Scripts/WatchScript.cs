633b7de198f6e8dd71749370edf1c66ddcf0f6b8
Maksim <Max_D_S@tut.by>
2021-10-04 22:22:07 +0300

    Added time counter

--- Assets/Scripts/WatchScript.cs
+++ Assets/Scripts/WatchScript.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class WatchScript : MonoBehaviour
 {
+    public static WatchScript Instance;
     [SerializeField] private TextMeshPro label;
     [SerializeField] private float timeMultiplier;
-    
+
+    [SerializeField] private float _startTimeInSeconds;
     [SerializeField] private float _timeInSeconds;
     private const float SecondsInDay = 60 * 60 * 24;
+
+    public float TimePassed => _timeInSeconds - _startTimeInSeconds;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        if (ScalebarManager.Instance.isFreezed)
+        {
+            return;
+        }
+        
         var timeSpan = TimeSpan.FromSeconds(_timeInSeconds);
         DateTime time = DateTime.Today.Add(timeSpan);
         string displayTime = time.ToString("hh:mm tt");
